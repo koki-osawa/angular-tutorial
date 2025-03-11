e1486c92bb2abf3224561f6d53ac2f8c78da23d2
Koki Osawa <kosawa0627@gmail.com>
2025-03-12 00:22:04 +0900

    update: details page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import {Component} from '@angular/core';
-import {HomeComponent} from './home/home.component';
 import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, RouterModule],
+  imports: [RouterModule],
   template: `
     <main>
       <a [routerLink]="['/']">
